c353e45e8ce3c64919fc40242ff696dfba221746
piotrstachecki <stachecki.piotr@gmail.com>
2025-04-08 18:52:18 +0200

    added spring security

---
+++ src/main/java/pl/bangly/carrental/controller/AdminController.java
@@ -0,0 +1,16 @@
+package pl.bangly.carrental.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/admin_page")
+    public String showAdminPage() {
+
+        return "admin-page";
+    }
+}
---
+++ src/main/java/pl/bangly/carrental/controller/LoginController.java
@@ -0,0 +1,20 @@
+package pl.bangly.carrental.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+
+        return "login-page";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+
+        return "access-denied";
+    }
+}
